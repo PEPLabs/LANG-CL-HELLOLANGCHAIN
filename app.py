7e7195f66038d00596c4154905b7676876f6857c
Theodore Balashov <theodore.balashov@revature.com>
2023-12-01 11:47:50 -0500

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+import langchain_core.messages.ai
+import requests
+
+from main.lab import lab
+
+"""
+This file will contain some sample code to send the output of the functions in lab.py to the 
+console. You may modify this file in any way, it will not affect the test results.
+"""
+
+
+def main():
+    print("Here is the present output of the lab() function's response, which should produce a JSON response from the "
+          "LLM.")
+    result = lab()
+    if type(result) is langchain_core.messages.ai.AIMessage:
+        print(result.content)
+    else:
+        print("the lab() function did not produce an AI message result.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/labtest.py
@@ -0,0 +1,47 @@
+"""
+This file will contain test cases for the automatic evaluation of your
+solution in main/lab.py. You should not modify the code in this file. You should
+also manually test your solution by running main/app.py.
+"""
+import os
+import unittest
+
+import langchain_core.messages.ai
+from langchain.chat_models import AzureChatOpenAI
+from main.lab import lab
+
+
+class TestLLMResponse(unittest.TestCase):
+    """
+    This test will verify that the connection to an external LLM is made. If it does not
+    work, this may be because the API key is invalid, or the service may be down.
+    If that is the case, this lab may not be completable.
+    """
+
+    def test_llm_sanity_check(self):
+        deployment = os.environ['DEPLOYMENT_NAME']
+        llm = AzureChatOpenAI(deployment_name=deployment, model_name="gpt-35-turbo")
+
+    """
+    The variable returned from the lab method should be an HTTP response, with a status
+    200. If the status is 400, then your request was incorrectly formatted.
+    """
+
+    def test_lab_ai_response(self):
+        result = lab()
+        self.assertIsInstance(result, langchain_core.messages.ai.AIMessage)
+
+    """
+    The JSON of the response returned from the lab method should contain "hello world"
+    (not case or punctuation sensitive.)
+    """
+
+    def test_hello_response(self):
+        result = lab()
+        content = result.content.lower()
+        self.assertIn("hello", content)  # Verifies if "hello" is present in the result
+        self.assertIn("llm", content)  # Verifies if "world" is present in the result
+
+
+if __name__ == '__main__':
+    unittest.main()
